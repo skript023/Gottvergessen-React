ea229535444aa9450f00b4cb967869ba2e14714b
skript023 <fatur_muhammad@bk.ru>
2022-12-01 15:48:20 +0700

    Added Hidden Text

--- src/view/core/footer.js
+++ src/view/core/footer.js
@@ -1,8 +1,9 @@
 
 const Footer = () => {
     return (
-        <footer className="py-5 bg-primary">
+        <footer className="py-2 bg-primary">
             <div className="container">
+                <p hidden>learn react</p>
                 <p className="m-0 text-center text-white">Copyright &copy; Fatur Muhammad 2022</p>
             </div>
         </footer>
