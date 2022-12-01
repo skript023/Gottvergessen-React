a6c5264a9e28d62e00bba3e4fa5a66dfbfb0609d
skript023 <fatur_muhammad@bk.ru>
2022-12-01 15:25:03 +0700

    Change class to Class name Footer

--- src/view/core/footer.js
+++ src/view/core/footer.js
@@ -1,9 +1,9 @@
 
 const Footer = () => {
     return (
-        <footer class="py-5 bg-primary">
-            <div class="container">
-                <p class="m-0 text-center text-white">Copyright &copy; Fatur Muhammad 2022</p>
+        <footer className="py-5 bg-primary">
+            <div className="container">
+                <p className="m-0 text-center text-white">Copyright &copy; Fatur Muhammad 2022</p>
             </div>
         </footer>
     );
